078016ba4933be43e504842bd8215d87232746a9
aidendef <aidendef@users.noreply.github.com>
2025-06-30 04:37:27 +0000

    deploy: fc522a0398bd76a82afac42cda8ae6fedd28c25e

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "첫 블로그 글",
-        "excerpt":"안녕하세요   Aiden의 든든한 블로그에 와주셔서 감사합니다. 앞으로 많이 찾아와주세요 :)  ","categories": ["private","blog"],
+        "excerpt":"안녕하세요   Aiden의 든든한 블로그에 와주셔서 감사합니다. 앞으로 많이 찾아와주세요 :)      Link Tree            LinkedIn       Blog       IEEE Profile       ORCID Profile          ","categories": ["private","blog"],
         "tags": ["blog"],
         "url": "/private/blog/welcome-to-my-blog/",
         "teaser": "/assets/images/teaser.webp"
@@ -18,92 +18,92 @@ var store = [{
         "teaser": "/assets/images/post_img/Inline-image-2025-02-18%2015.35.46.743.png"
       },{
         "title": "GCP vs AWS: AI 서비스 관점 비교",
-        "excerpt":"0. 앞서보는 3줄 결론 GCP는 AI/ML, 데이터 분석에 강하고, AWS는 서비스 다양성과 기업 클라우드에 최적화되어 있습니다. ➡ GCP는 AI 연구·개발용 클라우드, AWS는 기업용 AI 솔루션 클라우드입니다. 1. 출발점과 강점 비교 항목 GCP (Google Cloud Platform) AWS (Amazon Web Services) 출발 배경 구글 내부 인프라 기반 아마존 전자상거래 인프라 기반 강점...","categories": ["cloud","ai"],
+        "excerpt":"0. 앞서보는 3줄 결론 GCP는 AI/ML, 데이터 분석에 강하고, AWS는 서비스 다양성과 기업 클라우드에 최적화되어 있습니다. ➡ GCP는 AI 연구·개발용 클라우드, AWS는 기업용 AI 솔루션 클라우드입니다. 1. 출발점과 강점 비교 항목 GCP (Google Cloud Platform) AWS (Amazon Web Services) 출발 배경 구글 내부 인프라 기반 아마존 전자상거래 인프라 기반 강점...","categories": ["tech","cloud","ai"],
         "tags": ["GCP","AWS","AI","클라우드비교"],
-        "url": "/cloud/ai/GCPvsAWS%EB%B9%84%EA%B5%90/",
+        "url": "/tech/cloud/ai/GCPvsAWS%EB%B9%84%EA%B5%90/",
         "teaser": "/assets/images/post_img/ChatGPT%20Image%202025%EB%85%84%205%EC%9B%94%2012%EC%9D%BC%20%EC%98%A4%ED%9B%84%2003_17_30.png"
       },{
         "title": "AWS Bedrock 지식기반을 A to Z 모두 구현해보기",
-        "excerpt":"1. 들어가며 최근 다양한 기업들이 사내 문서 기반 지식 검색 시스템, 즉 RAG(Retrieval-Augmented Generation) 기반의 챗봇을 도입하고 있다. [Gartner 보고서 보기, Accenture AI 인사이트, McKinsey: 생성형 AI의 경제적 잠재력, AWS GenAI 공개 고객 사례집] 특히 AWS Bedrock은 Amazon Titan, Claude, Command 등을 통합 제공하며, LLM 기반 지식 응답 시스템을 완전관리형으로...","categories": ["cloud","ai","Business"],
+        "excerpt":"1. 들어가며 최근 다양한 기업들이 사내 문서 기반 지식 검색 시스템, 즉 RAG(Retrieval-Augmented Generation) 기반의 챗봇을 도입하고 있다. [Gartner 보고서 보기, Accenture AI 인사이트, McKinsey: 생성형 AI의 경제적 잠재력, AWS GenAI 공개 고객 사례집] 특히 AWS Bedrock은 Amazon Titan, Claude, Command 등을 통합 제공하며, LLM 기반 지식 응답 시스템을 완전관리형으로...","categories": ["tech","cloud","ai","Business"],
         "tags": ["AWS","AI","Bedrock","RAG","Retrieval-Augmented Generation","LLM","Knowledge Base","지식기반"],
-        "url": "/cloud/ai/business/AWS-Bedrock-%EC%A7%80%EC%8B%9D%EA%B8%B0%EB%B0%98%EC%9D%84-A-to-Z-%EB%AA%A8%EB%91%90-%EA%B5%AC%ED%98%84%ED%95%B4%EB%B3%B4%EA%B8%B0/",
+        "url": "/tech/cloud/ai/business/AWS-Bedrock-%EC%A7%80%EC%8B%9D%EA%B8%B0%EB%B0%98%EC%9D%84-A-to-Z-%EB%AA%A8%EB%91%90-%EA%B5%AC%ED%98%84%ED%95%B4%EB%B3%B4%EA%B8%B0/",
         "teaser": "/assets/images/post_img/Knowledge-Bases-feat-img.png"
       },{
         "title": "이쁨 받는 AWS 아키텍처 만들어보자",
-        "excerpt":"1. 들어가며 클라우드 환경에서의 아키텍처 설계는 단순히 서비스를 배치하는 것을 넘어, 확장성, 안정성, 비용 효율성까지 고려한 종합 설계이다. 하지만 처음 시작할 때는 수많은 AWS 서비스들 사이에서 무엇을 선택해야 할지, 어떻게 조합해야 할지 막막할 수밖에 없다. 이 글에서는 복잡한 이론 대신, 실제 현업에서도 이쁨(?) 받는 AWS 아키텍처를 만드는 실전 방식을 공유하려고...","categories": ["cloud"],
+        "excerpt":"1. 들어가며 클라우드 환경에서의 아키텍처 설계는 단순히 서비스를 배치하는 것을 넘어, 확장성, 안정성, 비용 효율성까지 고려한 종합 설계이다. 하지만 처음 시작할 때는 수많은 AWS 서비스들 사이에서 무엇을 선택해야 할지, 어떻게 조합해야 할지 막막할 수밖에 없다. 이 글에서는 복잡한 이론 대신, 실제 현업에서도 이쁨(?) 받는 AWS 아키텍처를 만드는 실전 방식을 공유하려고...","categories": ["tech","cloud"],
         "tags": ["AWS","AI","Bedrock","Sagemaker","Architecture","Cloud","보안"],
-        "url": "/cloud/%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8%EC%9D%98-%EA%B8%B0%EC%B4%88-AWS-%EC%95%84%ED%82%A4%ED%85%8D%EC%B2%98-%EB%A7%8C%EB%93%A4%EC%96%B4%EB%B3%B4%EC%9E%90/",
+        "url": "/tech/cloud/%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8%EC%9D%98-%EA%B8%B0%EC%B4%88-AWS-%EC%95%84%ED%82%A4%ED%85%8D%EC%B2%98-%EB%A7%8C%EB%93%A4%EC%96%B4%EB%B3%B4%EC%9E%90/",
         "teaser": "/assets/images/post_img/archi-sample.png"
       },{
         "title": "AWS Summit Seoul 2025 탐방기",
-        "excerpt":"1. 클라우드의 끝이 아닌, 시작점 이번 서밋에서 AWS는 더 이상 단순한 인프라 기업이 아니었다. AI, 서버리스, 지속 가능성까지… 그들의 새로운 ‘무기’는 기존의 패러다임을 완전히 전복시키는 인상을 주었다. 특히 AWS가 강조한 방향성은 ‘모든 기업이 생성형 AI를 바로 실무에 도입할 수 있는 실용성 중심’이었다. 대표적인 키노트와 세션 주제들은 하나같이 생성형 AI에 집중되어...","categories": ["cloud"],
+        "excerpt":"1. 클라우드의 끝이 아닌, 시작점 이번 서밋에서 AWS는 더 이상 단순한 인프라 기업이 아니었다. AI, 서버리스, 지속 가능성까지… 그들의 새로운 ‘무기’는 기존의 패러다임을 완전히 전복시키는 인상을 주었다. 특히 AWS가 강조한 방향성은 ‘모든 기업이 생성형 AI를 바로 실무에 도입할 수 있는 실용성 중심’이었다. 대표적인 키노트와 세션 주제들은 하나같이 생성형 AI에 집중되어...","categories": ["tech","ai","cloud"],
         "tags": ["AWS","2025","Summit","코엑스"],
-        "url": "/cloud/AWS-2025-Summit-%ED%83%90%EB%B0%A9%EA%B8%B0/",
+        "url": "/tech/ai/cloud/AWS-2025-Summit-%ED%83%90%EB%B0%A9%EA%B8%B0/",
         "teaser": "/assets/images/post_img/IMG_4627%20(2).gif"
       },{
         "title": "ALB, NLB, CLB, GWLB – AWS 로드 밸런서 완전 정리",
-        "excerpt":"1. 들어가며 AWS에서 서비스를 운영하다 보면, 로드 밸런서는 거의 필수다. 하지만 ELB 안에도 종류가 4가지나 있어서 처음 접하면 헷갈리기 쉽다. 웹 서비스엔 ALB? 게임 서버엔 NLB? 옛날 시스템은 CLB? 방화벽 연결엔 GWLB? 이번 글에서는 AWS의 로드 밸런서 종류를 한 번에 정리해보고, 각 로드 밸런서가 어떤 상황에 적합한지 사례를 들어가며 설명해보려고...","categories": ["cloud"],
+        "excerpt":"1. 들어가며 AWS에서 서비스를 운영하다 보면, 로드 밸런서는 거의 필수다. 하지만 ELB 안에도 종류가 4가지나 있어서 처음 접하면 헷갈리기 쉽다. 웹 서비스엔 ALB? 게임 서버엔 NLB? 옛날 시스템은 CLB? 방화벽 연결엔 GWLB? 이번 글에서는 AWS의 로드 밸런서 종류를 한 번에 정리해보고, 각 로드 밸런서가 어떤 상황에 적합한지 사례를 들어가며 설명해보려고...","categories": ["tech","cloud"],
         "tags": ["AWS","ALB","NLB","CLB","GWLB","로드밸런서"],
-        "url": "/cloud/ALB,-NLB,-CLB,-GWLB-AWS-%EB%A1%9C%EB%93%9C-%EB%B0%B8%EB%9F%B0%EC%84%9C-%EC%99%84%EC%A0%84-%EC%A0%95%EB%A6%AC/",
+        "url": "/tech/cloud/ALB,-NLB,-CLB,-GWLB-AWS-%EB%A1%9C%EB%93%9C-%EB%B0%B8%EB%9F%B0%EC%84%9C-%EC%99%84%EC%A0%84-%EC%A0%95%EB%A6%AC/",
         "teaser": "/assets/images/post_img/%EB%A1%9C%EB%93%9C%EB%B0%B8%EB%9F%B0%EC%84%9C.png"
       },{
         "title": "Agentic_Workflow_using_LangGraph_and_Bedrock(1)",
-        "excerpt":"간단한 그래프 LangGraph에서 개념을 갖고 있는 Graph에 대해서 알아보기 위해서, 아주 간단한 그래프를 그려볼 것입니다. 간단한 3개의 노드를 이용해서 그래프를 그려볼 수 있습니다. 패키지 설치 %%capture --no-stderr %pip install --quiet -U langgraph State (상태저장) 우선, 그래프의 상태를 저장하는 State class를 우선 정의합니다. 이 상태 스키마는 그래프의 모든 노드에 대한 정보들을...","categories": ["ai","cloud"],
+        "excerpt":"간단한 그래프 LangGraph에서 개념을 갖고 있는 Graph에 대해서 알아보기 위해서, 아주 간단한 그래프를 그려볼 것입니다. 간단한 3개의 노드를 이용해서 그래프를 그려볼 수 있습니다. 패키지 설치 %%capture --no-stderr %pip install --quiet -U langgraph State (상태저장) 우선, 그래프의 상태를 저장하는 State class를 우선 정의합니다. 이 상태 스키마는 그래프의 모든 노드에 대한 정보들을...","categories": ["tech","ai","cloud"],
         "tags": ["AWS","Agentic","LangGraph","Bedrock"],
-        "url": "/ai/cloud/Agentic_Workflow_using_LangGraph_and_Bedrock(1)/",
+        "url": "/tech/ai/cloud/Agentic_Workflow_using_LangGraph_and_Bedrock(1)/",
         "teaser": "/assets/images/post_img/langgraph1.png"
       },{
         "title": "Agentic_Workflow_using_LangGraph_and_Bedrock(2)",
-        "excerpt":"라우터란? 사용자의 인풋에 따라서, Gen AI를 컨텍스트에 맞는 답변을 수행해야 하는지를 판단하게 할 수 있습니다. 이러한 작업을 수행하는 것이 라우터입니다. 그래서 사용자의 컨텍스트에 맞게 해당되는 툴로 답변을 유도하거나, LLM이 스스로 답변하는 형태로 답변하도록 합니다. 이 실습예제도 사용자의 요청에 따라서 툴을 사용하거난, LLM을 통해서 직접 답변하게는 등의 수행이 제대로 되는지를 실습해...","categories": ["ai","cloud"],
+        "excerpt":"라우터란? 사용자의 인풋에 따라서, Gen AI를 컨텍스트에 맞는 답변을 수행해야 하는지를 판단하게 할 수 있습니다. 이러한 작업을 수행하는 것이 라우터입니다. 그래서 사용자의 컨텍스트에 맞게 해당되는 툴로 답변을 유도하거나, LLM이 스스로 답변하는 형태로 답변하도록 합니다. 이 실습예제도 사용자의 요청에 따라서 툴을 사용하거난, LLM을 통해서 직접 답변하게는 등의 수행이 제대로 되는지를 실습해...","categories": ["tech","ai","cloud"],
         "tags": ["AWS","Agentic","LangGraph","Bedrock"],
-        "url": "/ai/cloud/Agentic_Workflow_using_LangGraph_and_Bedrock(2)/",
+        "url": "/tech/ai/cloud/Agentic_Workflow_using_LangGraph_and_Bedrock(2)/",
         "teaser": "/assets/images/post_img/langgraph2.png"
       },{
         "title": "Agentic_Workflow_using_LangGraph_and_Bedrock(3)",
-        "excerpt":"툴 에이전트 실습 앞에서 라우터를 실습해 봤기 때문에, 여기에서는 좀 더 해당 개념을 확장해서 수행할 예정입니다. 우리는 앞의 라우터 실습에서, LLM에게 질의를 하였고, 만약에 질의의 내용이 툴을 호출하는 것이라면, ToolMessage를 통해서 답변을 하는 것을 보았습니다. 이번에는, ToolMessage를 바로 사용자에게 답변하는 것이 아니라, 이 메세지를 다시 모델에게 전달할 수 있을까요? 그래서...","categories": ["ai","cloud"],
+        "excerpt":"툴 에이전트 실습 앞에서 라우터를 실습해 봤기 때문에, 여기에서는 좀 더 해당 개념을 확장해서 수행할 예정입니다. 우리는 앞의 라우터 실습에서, LLM에게 질의를 하였고, 만약에 질의의 내용이 툴을 호출하는 것이라면, ToolMessage를 통해서 답변을 하는 것을 보았습니다. 이번에는, ToolMessage를 바로 사용자에게 답변하는 것이 아니라, 이 메세지를 다시 모델에게 전달할 수 있을까요? 그래서...","categories": ["tech","ai","cloud"],
         "tags": ["AWS","Agentic","LangGraph","Bedrock"],
-        "url": "/ai/cloud/Agentic_Workflow_using_LangGraph_and_Bedrock(3)/",
+        "url": "/tech/ai/cloud/Agentic_Workflow_using_LangGraph_and_Bedrock(3)/",
         "teaser": "/assets/images/post_img/langgraph3.png"
       },{
         "title": "Agentic_Workflow_using_LangGraph_and_Bedrock(4)",
-        "excerpt":"그래프 메모리란? Agent를 통해서 처리되는 것들을 살펴 보았습니다. 하지만, LLM은 기본적으로 사용자가 수행하는 대화를 기억하지 못합니다. 그래서 그래프에서도 사용자가 수행하는 대화를 기억할 수 있도록 하는 장치가 필요합니다. 그래서 그래프 메모리는 이렇게 사용자가 수행한 대화를 기억할 수 있도록 도와서 컨텍스트에 맞는 답변을 하도록 도움을 줍니다. Bedrock setup import os import getpass...","categories": ["ai","cloud"],
+        "excerpt":"그래프 메모리란? Agent를 통해서 처리되는 것들을 살펴 보았습니다. 하지만, LLM은 기본적으로 사용자가 수행하는 대화를 기억하지 못합니다. 그래서 그래프에서도 사용자가 수행하는 대화를 기억할 수 있도록 하는 장치가 필요합니다. 그래서 그래프 메모리는 이렇게 사용자가 수행한 대화를 기억할 수 있도록 도와서 컨텍스트에 맞는 답변을 하도록 도움을 줍니다. Bedrock setup import os import getpass...","categories": ["tech","ai","cloud"],
         "tags": ["AWS","Agentic","LangGraph","Bedrock"],
-        "url": "/ai/cloud/Agentic_Workflow_using_LangGraph_and_Bedrock(4)/",
+        "url": "/tech/ai/cloud/Agentic_Workflow_using_LangGraph_and_Bedrock(4)/",
         "teaser": "/assets/images/post_img/langgraph4.png"
       },{
         "title": "Agentic_Workflow_using_LangGraph_and_Bedrock(5)",
-        "excerpt":"상태 스키마 이 모듈에서는 상태를 저장하는 스키마와 이와 연관된 메모리에 대해서 좀 더 알아보겠습니다. Bedrock Setting import os import getpass import boto3 from langchain_aws import ChatBedrockConverse from langchain_aws import ChatBedrock # ---- ⚠️ Update region for your AWS setup ⚠️ ---- aws_region = os.getenv(\"AWS_REGION\") bedrock_client = boto3.client(\"bedrock-runtime\", region_name=aws_region) llm =...","categories": ["ai","cloud"],
+        "excerpt":"상태 스키마 이 모듈에서는 상태를 저장하는 스키마와 이와 연관된 메모리에 대해서 좀 더 알아보겠습니다. Bedrock Setting import os import getpass import boto3 from langchain_aws import ChatBedrockConverse from langchain_aws import ChatBedrock # ---- ⚠️ Update region for your AWS setup ⚠️ ---- aws_region = os.getenv(\"AWS_REGION\") bedrock_client = boto3.client(\"bedrock-runtime\", region_name=aws_region) llm =...","categories": ["tech","ai","cloud"],
         "tags": ["AWS","Agentic","LangGraph","Bedrock"],
-        "url": "/ai/cloud/Agentic_Workflow_using_LangGraph_and_Bedrock(5)/",
+        "url": "/tech/ai/cloud/Agentic_Workflow_using_LangGraph_and_Bedrock(5)/",
         "teaser": "/assets/images/post_img/langgraph5.png"
       },{
         "title": "Agentic_Workflow_using_LangGraph_and_Bedrock(6)",
-        "excerpt":"Human in the Loop 이제 휴먼 인 더 루프가 필요한 배경에 대해 이야기해 보겠습니다: (1) 승인 - 에이전트를 중단하고 사용자에게 상태를 표시하고 사용자가 작업을 수락하도록 허용할 수 있습니다. (2) 디버깅 - 그래프를 되감아 문제를 재현하거나 피할 수 있습니다. (3) 편집 - 상태를 수정할 수 있습니다. LangGraph는 다양한 휴먼 인 더...","categories": ["ai","cloud"],
+        "excerpt":"Human in the Loop 이제 휴먼 인 더 루프가 필요한 배경에 대해 이야기해 보겠습니다: (1) 승인 - 에이전트를 중단하고 사용자에게 상태를 표시하고 사용자가 작업을 수락하도록 허용할 수 있습니다. (2) 디버깅 - 그래프를 되감아 문제를 재현하거나 피할 수 있습니다. (3) 편집 - 상태를 수정할 수 있습니다. LangGraph는 다양한 휴먼 인 더...","categories": ["tech","ai","cloud"],
         "tags": ["AWS","Agentic","LangGraph","Bedrock"],
-        "url": "/ai/cloud/Agentic_Workflow_using_LangGraph_and_Bedrock(6)/",
+        "url": "/tech/ai/cloud/Agentic_Workflow_using_LangGraph_and_Bedrock(6)/",
         "teaser": "/assets/images/post_img/langgraph6.png"
       },{
         "title": "Agentic_Workflow_using_LangGraph_and_Bedrock(7)",
-        "excerpt":"Human in the Loop 변경 중단점이 사용자 승인을 지원하는 방법을 보여드렸지만 그래프가 중단된 후 그래프 상태를 수정하는 방법에 대해서는 아직 설명드리지 않았습니다. 이제 그래프 상태를 직접 편집하고 사람의 피드백을 입력하는 방법을 보여드리겠습니다. Bedrock setup import os import getpass import boto3 from langchain_aws import ChatBedrockConverse from langchain_aws import ChatBedrock # ----...","categories": ["ai","cloud"],
+        "excerpt":"Human in the Loop 변경 중단점이 사용자 승인을 지원하는 방법을 보여드렸지만 그래프가 중단된 후 그래프 상태를 수정하는 방법에 대해서는 아직 설명드리지 않았습니다. 이제 그래프 상태를 직접 편집하고 사람의 피드백을 입력하는 방법을 보여드리겠습니다. Bedrock setup import os import getpass import boto3 from langchain_aws import ChatBedrockConverse from langchain_aws import ChatBedrock # ----...","categories": ["tech","ai","cloud"],
         "tags": ["AWS","Agentic","LangGraph","Bedrock"],
-        "url": "/ai/cloud/Agentic_Workflow_using_LangGraph_and_Bedrock(7)/",
+        "url": "/tech/ai/cloud/Agentic_Workflow_using_LangGraph_and_Bedrock(7)/",
         "teaser": "/assets/images/post_img/langgraph7.png"
       },{
         "title": "Agentic_Workflow_using_LangGraph_and_Bedrock(8)",
-        "excerpt":"동적인 Breakpoint 특정 단계에서 그래프를 멈추는 일반적인 방법으로 중단점을 다루었으며, 이를 통해 ‘승인’과 같은 사용 사례를 구현할 수 있습니다. 또한 그래프 상태를 편집하는 방법과 사람의 피드백을 도입하는 방법도 소개했습니다. 중단점은 개발자가 그래프를 컴파일하는 동안 특정 노드에 설정합니다. 하지만 때로는 그래프가 동적으로 중단되도록 하는 것이 유용할 때가 있습니다! 이것은 내부 중단점이며,...","categories": ["ai","cloud"],
+        "excerpt":"동적인 Breakpoint 특정 단계에서 그래프를 멈추는 일반적인 방법으로 중단점을 다루었으며, 이를 통해 ‘승인’과 같은 사용 사례를 구현할 수 있습니다. 또한 그래프 상태를 편집하는 방법과 사람의 피드백을 도입하는 방법도 소개했습니다. 중단점은 개발자가 그래프를 컴파일하는 동안 특정 노드에 설정합니다. 하지만 때로는 그래프가 동적으로 중단되도록 하는 것이 유용할 때가 있습니다! 이것은 내부 중단점이며,...","categories": ["tech","ai","cloud"],
         "tags": ["AWS","Agentic","LangGraph","Bedrock"],
-        "url": "/ai/cloud/Agentic_Workflow_using_LangGraph_and_Bedrock(8)/",
+        "url": "/tech/ai/cloud/Agentic_Workflow_using_LangGraph_and_Bedrock(8)/",
         "teaser": "/assets/images/post_img/langgraph8.png"
       },{
         "title": "나, 내 팀원이 가졌으면 하는 개발자 마인드",
-        "excerpt":"개발자 마인드란? 사실 개발을 진행하면서 개발실력이 중요하지만 서로서로 의견을 소통하고 교류하는 것이 더 크다. 이럴때 우리는 어떤 마인드를 가지며 살아가야할까? 아래 다른 포스팅이나 유튜브 등의 글을 지속적으로 업데이트할 예정이다. 링크 모음 토스의 8가지 라이팅 원칙들 주니어 개발자 마인드셋 생각과 손 사이의 거리 평범한 엔지니어를 찬양하며 언제 멈추고, 언제 밀어붙여야 할까?...","categories": ["private","blog"],
+        "excerpt":"개발자 마인드란? 사실 개발을 진행하면서 개발실력이 중요하지만 서로서로 의견을 소통하고 교류하는 것이 더 크다. 이럴때 우리는 어떤 마인드를 가지며 살아가야할까? 아래 다른 포스팅이나 유튜브 등의 글을 지속적으로 업데이트할 예정이다. 링크 모음 토스의 8가지 라이팅 원칙들 주니어 개발자 마인드셋 생각과 손 사이의 거리 평범한 엔지니어를 찬양하며 언제 멈추고, 언제 밀어붙여야 할까?...","categories": ["tech","private","blog"],
         "tags": ["Problem Framing","junior","developer","pm","writing","toss"],
-        "url": "/private/blog/%EA%B0%9C%EB%B0%9C%EC%9E%90_%EB%A7%88%EC%9D%B8%EB%93%9C-copy/",
+        "url": "/tech/private/blog/%EA%B0%9C%EB%B0%9C%EC%9E%90_%EB%A7%88%EC%9D%B8%EB%93%9C-copy/",
         "teaser": "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExb2ZuajZpdXpkcGVtbW03NjRianh5Nm56bWkxZmJ3OHV2bnh1czgxMSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/6aQ3Hy4i90nGdRnG8i/giphy.gif"
       },{
         "title": "AI 완전 자동화? 절반은 원하지 않는다 - 우리가 진짜 준비해야 할 역량",
-        "excerpt":"1. 왜 이 논문을 알아야하나? 얼마나 중요한가? Future of Work with AI Agents 라는 논문을 읽고 아래 간략하게 작성을 진행할 것이다. 긴글을 읽기 앞서 왜 우리는 이 글을 봐야하는지 설명을 진행할 것이다. AI가 인간 노동을 대체할 것이라는 통념의 재검토 최근 AI, 특히 생성형 AI와 에이전트 시스템의 발전은 “AI가 인간의 일을...","categories": ["ai"],
+        "excerpt":"1. 왜 이 논문을 알아야하나? 얼마나 중요한가? Future of Work with AI Agents 라는 논문을 읽고 아래 간략하게 작성을 진행할 것이다. 긴글을 읽기 앞서 왜 우리는 이 글을 봐야하는지 설명을 진행할 것이다. AI가 인간 노동을 대체할 것이라는 통념의 재검토 최근 AI, 특히 생성형 AI와 에이전트 시스템의 발전은 “AI가 인간의 일을...","categories": ["tech","ai","private","blog"],
         "tags": ["Human-Centric AI","AI Collaboration","Future of Work","Human Agency","Augmentation over Automation","Future of Work with AI Agents"],
-        "url": "/ai/AI-%EC%99%84%EC%A0%84-%EC%9E%90%EB%8F%99%ED%99%94-%EC%A0%88%EB%B0%98%EC%9D%80-%EC%9B%90%ED%95%98%EC%A7%80-%EC%95%8A%EB%8A%94%EB%8B%A4-%EC%9A%B0%EB%A6%AC%EA%B0%80-%EC%A7%84%EC%A7%9C-%EC%A4%80%EB%B9%84%ED%95%B4%EC%95%BC-%ED%95%A0-%EC%97%AD%EB%9F%89-copy/",
+        "url": "/tech/ai/private/blog/AI-%EC%99%84%EC%A0%84-%EC%9E%90%EB%8F%99%ED%99%94-%EC%A0%88%EB%B0%98%EC%9D%80-%EC%9B%90%ED%95%98%EC%A7%80-%EC%95%8A%EB%8A%94%EB%8B%A4-%EC%9A%B0%EB%A6%AC%EA%B0%80-%EC%A7%84%EC%A7%9C-%EC%A4%80%EB%B9%84%ED%95%B4%EC%95%BC-%ED%95%A0-%EC%97%AD%EB%9F%89-copy/",
         "teaser": "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExeDV5OXFubHBnZ252ZGt6d21hcnVzZjNpZ21lN2UzdWc0MDBsdTFjbCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/ABVK96HgZvWI9SBbXr/giphy.gif"
       }]
